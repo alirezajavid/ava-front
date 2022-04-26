40c3e9d7f80589a2844a32d34ddd047eaca99f70
Alireza  Javidtajrishi <javid@MacBook-Pro.local>
2022-04-26 19:20:06 +0430

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+
+        <q-toolbar-title class="absolute-center">
+          بازی آوا
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- Now obviously not like this : ) but how ? -->
+    <transition appear enter-active-class="animated bounceInDown"
+    leave-active-class="animated flipOutY" :duration="2000  0">
+      <q-card
+        v-show="show"
+        style="
+        background-color: #FCFFE7;
+        width: 600px;
+        background: url(/b1.png) no-repeat;
+        "
+      >
+        <div style="text-align:center; margin-top:40px; font-size: 24px; ">
+          مرحله اول
+        </div>
+        <div style="direction:rtl; text-align:right; margin-top:20px; padding: 40px; font-size:18px;">
+          سلام آوا عزیز
+        </div>
+      </q-card>
+    </transition>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      show: false
+    }
+  },
+  mounted() {
+    setTimeout(() => { this.show = !this.show}, 2000)
+  }
+}
+</script>
